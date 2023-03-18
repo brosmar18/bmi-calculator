0a47354cd84f7991061181a53fa6ff372798d8fe
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-03-18 18:48:10 -0500

    pounds converted to kg

---
+++ js/app.js
@@ -0,0 +1,9 @@
+console.log('I am  currently 170 lbs, and I\'m 5.7ft tall. Let\'s calculate my BMI!');
+
+// convert pounds to kg.
+
+let pounds = 170;
+
+let kg = 170 / 2.2;
+
+kgRounded = kg.toFixed(2);
