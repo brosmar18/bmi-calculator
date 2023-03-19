7fdcf70692118e5b88d90449c331f67761c6869b
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-03-18 19:02:31 -0500

    feet converted to inches

--- js/app.js
+++ js/app.js
@@ -4,6 +4,12 @@ console.log('I am  currently 170 lbs, and I\'m 5.7ft tall. Let\'s calculate my B
 
 let pounds = 170;
 
-let kg = 170 / 2.2;
+let kg = pounds / 2.2046;
 
-kgRounded = kg.toFixed(2);
+console.log(`My weight is: ${kg.toFixed(1)} kg.`);
+
+let heightFeet = 5.7;
+
+let heightInches = heightFeet * 12;
+
+console.log(`My height is: ${heightInches.toFixed(1)} inches.`);
