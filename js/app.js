f80816cd0325173fed4fb7967419dfca55f4e09b
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-03-18 19:02:52 -0500

    BMI calculated

--- js/app.js
+++ js/app.js
@@ -6,10 +6,20 @@ let pounds = 170;
 
 let kg = pounds / 2.2046;
 
-console.log(`My weight is: ${kg.toFixed(1)} kg.`);
+console.log(`My weight in kg is: ${kg.toFixed(1)}.`);
 
 let heightFeet = 5.7;
 
 let heightInches = heightFeet * 12;
 
-console.log(`My height is: ${heightInches.toFixed(1)} inches.`);
+console.log(`My height in inches is: ${heightInches.toFixed(1)}`);
+
+let heightCm = heightInches * 2.54;
+
+let heightMeters = heightCm * 0.01;
+
+console.log(`My height in meters is: ${heightMeters.toFixed(1)}`);
+
+let bmi = kg / heightMeters ** 2;
+
+console.log(`My BMI is: ${bmi.toFixed(1)}`);
